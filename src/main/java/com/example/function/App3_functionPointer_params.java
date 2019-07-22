08cef589cc4fdcc2ab9625ae941ef868b5f79823
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2019-07-22 19:39:13 +0700

    update

--- src/main/java/com/example/function/App3_functionPointer_params.java
+++ src/main/java/com/example/function/App3_functionPointer_params.java
@@ -14,12 +14,7 @@ public class App3_functionPointer_params {
 	public static void main(String[] args) {
 		
 		//dùng Lambda syntax => dùng như pointer function trên C++
-/*		functionHandler(App3_functionPointer_params::functionPointer1);  // 
-
-		functionHandler(App3_functionPointer_params::functionPointer2);*/
-		
-		
-		functionHandler(App2_functionPointer::functionPointer3); 
+		functionHandler(App3_functionPointer_params::functionPointer3);
 	}
 
 	// define input cua funtion la 1 @FunctionalInterface
@@ -27,15 +22,7 @@ public class App3_functionPointer_params {
     	handler.handle("param1","param2");
     }
 
-    
-    static void functionPointer1(String st) {
-    	System.out.println("functionPointer1:"+st);
-    }
 
-
-    static void functionPointer2(String st) {
-    	System.out.println("functionPointer2:"+st);
-    }
     
     static void functionPointer3(String st1, String st2) {
     	System.out.println("functionPointer2:"+st1 + ":" + st2);
